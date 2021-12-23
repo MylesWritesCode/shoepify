6bd3d86e8eb52c7b90334aad9f912ebfc538a3cb
Myles Berueda <myles.berueda@codemithril.com>
2021-12-23 09:17:46 -1000

    impl: @shopify/hydrogen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/**
+ * File: /src/pages/_app.tsx
+ * Project: govalo-store-playground
+ * Purpose: // TODO: Add the purpose of the file here.
+ *
+ * @author Myles Berueda
+ * @date   Tuesday December 21st 2021
+ * -----
+ * Modified: Tuesday December 21st 2021 8:21:59 am
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ **/
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ShopifyServerProvider, DefaultRoutes } from "@shopify/hydrogen";
+
+import shopifyConfig from "../../shopify.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ShopifyServerProvider shopifyConfig={shopifyConfig} >
+      <Component {...pageProps} />
+    </ShopifyServerProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
