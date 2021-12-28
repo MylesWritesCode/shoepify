b18a64ecf135b4e599e2424ba7cfe264edd39bd5
Myles Berueda <myles.berueda@codemithril.com>
2021-12-28 10:55:49 -1000

    changed data type for mock product

--- src/mock/default-product.ts
+++ src/mock/default-product.ts
@@ -1,7 +1,7 @@
 /**
  * File: /src/mock/default-product.ts
  * Project: govalo-store-playground
- * Purpose: // TODO: Add the purpose of the file here.
+ * Purpose: Default product for use in product page development
  *
  * @author Myles Berueda
  * @date   Tuesday December 28th 2021
@@ -17,6 +17,6 @@ export default {
   company: "Sneaker Company",
   description:
     "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
-  price: "250.00",
-  discount: "0.50",
+  price: 250.00,
+  discount: 0.50,
 };
