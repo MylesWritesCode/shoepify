1fa661eeec72dbd191bf9973af8d171b668b6a2d
Myles Berueda <myles.berueda@codemithril.com>
2021-12-28 10:17:53 -1000

    added default product mock data

---
+++ src/mock/default-product.ts
@@ -0,0 +1,22 @@
+/**
+ * File: /src/mock/default-product.ts
+ * Project: govalo-store-playground
+ * Purpose: // TODO: Add the purpose of the file here.
+ *
+ * @author Myles Berueda
+ * @date   Tuesday December 28th 2021
+ * -----
+ * Modified: Tuesday December 28th 2021 9:36:16 am
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ **/
+export default {
+  title: "Fall Limited Edition Sneakers",
+  company: "Sneaker Company",
+  description:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+  price: "250.00",
+  discount: "0.50",
+};
