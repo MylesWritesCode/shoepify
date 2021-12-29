75ae0ecd1d29b9975de51a3c7ca523bba66e3501
Myles Berueda <myles.berueda@codemithril.com>
2021-12-29 13:49:27 -1000

    fixed jsdoc

--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -1,24 +1,24 @@
 /**
  * File: /src/types/product.type.ts
- * Project: govalo-store-playground
- * Purpose: Contains type information for products
+ * Project: shoepify
+ * Purpose: Contains a product data type
  * 
  * @author Myles Berueda
  * @date   Tuesday December 28th 2021
- * -----
+ * *****
  * Modified: Tuesday December 28th 2021 9:37:58 am
- * -----
+ * *****
  * Copyright (c) 2021 MylesWritesCode
- * -----
+ * *****
  * HISTORY
 **/
 
 /** Product Type
- * @param { title } string
- * @param { company } string
- * @param { description } string
- * @param { price } number The price in number form
- * @param { discount } number The percentage discount in decimal form.
+ * @param { string } title 
+ * @param { string } company 
+ * @param { string } description 
+ * @param { number } price - The price in number form
+ * @param { number } discount - The percentage discount in decimal form.
  */
 export interface Product {
   title: string;
