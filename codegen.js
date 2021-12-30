93b1cdccd651b2686ba712c437a3650598921519
Myles Berueda <myles.berueda@codemithril.com>
2021-12-30 13:41:39 -1000

    docs for codegen

--- codegen.js
+++ codegen.js
@@ -1,19 +1,18 @@
 /**
  * File: /codegen.js
- * Project: govalo-store-playground
- * Purpose: I want to use a JS file instead of YAML, cause we need stuff from
- *          shopify.config.js. Make sure that file is modified with the right
- *          stuff.
- *
+ * Project: shoepify
+ * Purpose: Config for graphql-codegen. Updated to use env vars instead of 
+ *          pulling from `shopify.config.js`
+ * 
  * @author Myles Berueda
  * @date   Wednesday December 22nd 2021
- * -----
+ * *****
  * Modified: Wednesday December 22nd 2021 10:23:07 am
- * -----
+ * *****
  * Copyright (c) 2021 MylesWritesCode
- * -----
+ * *****
  * HISTORY
- **/
+**/
 const dotenv = require('dotenv');
 dotenv.config();
 
