16ea0199fb9c15351a31f79e026411b4bee37d75
Myles Berueda <myles.berueda@codemithril.com>
2022-01-07 08:38:08 -1000

    scaffold: cart stuff

---
+++ src/components/navbar/Cart.tsx
@@ -0,0 +1,29 @@
+/**
+ * File: /src/components/navbar/Cart.tsx
+ * Project: shoepify
+ * Purpose: Displays the cart as a popup tooltip
+ *
+ * @author Myles Berueda
+ * @date   Friday January 7th 2022
+ * *****
+ * Modified: Friday January 7th 2022 8:33:58 am
+ * *****
+ * Copyright (c) 2022 MylesWritesCode
+ * *****
+ * HISTORY
+ **/
+import React from "react";
+
+interface CartProps {}
+
+const Cart: React.FC<CartProps> = ({ ...props }) => {
+  return (
+    <div>
+      <div>
+        <div>testing</div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
